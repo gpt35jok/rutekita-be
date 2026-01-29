0605d33d0aa0332d5c5069e7df1fdf9fcbdbff82
Heri Setyawan <gpt35mail@gmail.com>
2026-01-29 19:58:27 +0700

    fix authentication postgres

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
     SECRET_KEY = "super-secret-key"
-    SQLALCHEMY_DATABASE_URI = "postgresql://rutekita_user:rutekita_user@localhost:5432/rutekita"
+    SQLALCHEMY_DATABASE_URI = "postgresql://rutekita:rutekita@localhost:5432/rutekita"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = "jwt-secret-key"
